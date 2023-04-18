7cfefce4d2f99d5036a24fed96da6196d8347a6c
Reinan <reinanzinho100@gmail.com>
2023-04-17 23:20:02 -0300

    caminho no tabuleiro mapeado

--- src/front_end/pecas.java
+++ src/front_end/pecas.java
@@ -5,19 +5,18 @@ import java.util.concurrent.TimeUnit;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 
-public class pecas extends Thread{
+public class pecas extends Thread {
 	private JLabel peca;
 	private int numDado;
 	private String nomeImg;
 	private int numPlay;
 	private String nomePlay;
 	private ImageIcon img;
-	
+
 	private int posX;
 	private int posY;
-	
-	
-	public pecas(JLabel peca/*, int numDado, String nomeImg, int numPlay, String nomePlay*/) {
+
+	public pecas(JLabel peca/* , int numDado, String nomeImg, int numPlay, String nomePlay */) {
 		super();
 		this.peca = peca;
 		this.numDado = numDado;
@@ -29,28 +28,129 @@ public class pecas extends Thread{
 	public void run() {
 		jogada(numDado, peca);
 	}
-	
+
 	public void jogada(int numDado, JLabel peca) {
-		posX = peca.getX();
-		posY = peca.getY();
-		
-		for(int i = 0 ; i < 31 ; i++) {
-			sleep();
-			if(i%2 == 0) {
-				img = new ImageIcon(Panel.class.getResource("/resources/kankuro_ataque1.png"));
-			}else {
-				img = new ImageIcon(Panel.class.getResource("/resources/kankuro_ataque2.png"));
+		posX = peca.getX() - 88;
+		posY = peca.getY() - 58;
+		for (int i = 0; i < 58; i++) {
+
+			if (i % 2 == 0) {
+				img = new ImageIcon(Panel.class.getResource("/resources/kankuro_frente1.png"));
+			} else {
+				img = new ImageIcon(Panel.class.getResource("/resources/kankuro_frente2.png"));
 			}
+
+			if (i <= 5) {
+				posY += 34;
+			}
+			if (i >= 5 && i < 11) {
+				if (i % 2 == 0) {
+					img = new ImageIcon(Panel.class.getResource("/resources/kankuro_direita1.png"));
+				} else {
+					img = new ImageIcon(Panel.class.getResource("/resources/kankuro_direita2.png"));
+				}
+				posX += 33;
+			}
+			if(i >= 11 && i < 13) {
+				if (i % 2 == 0) {
+					img = new ImageIcon(Panel.class.getResource("/resources/kankuro_frente1.png"));
+				} else {
+					img = new ImageIcon(Panel.class.getResource("/resources/kankuro_frente2.png"));
+				}
+				posY += 34;
+			}
+			if(i >= 13 && i <= 18) {
+				if (i % 2 == 0) {
+					img = new ImageIcon(Panel.class.getResource("/resources/kankuro_esquerda1.png"));
+				} else {
+					img = new ImageIcon(Panel.class.getResource("/resources/kankuro_esquerda2.png"));
+				}
+				posX -= 33;
+			}
+			if(i >= 18 && i < 24) {
+				if (i % 2 == 0) {
+					img = new ImageIcon(Panel.class.getResource("/resources/kankuro_frente1.png"));
+				} else {
+					img = new ImageIcon(Panel.class.getResource("/resources/kankuro_frente2.png"));
+				}
+				posY += 34;
+			}
+			if(i >= 24 && i < 26) {
+				if (i % 2 == 0) {
+					img = new ImageIcon(Panel.class.getResource("/resources/kankuro_esquerda1.png"));
+				} else {
+					img = new ImageIcon(Panel.class.getResource("/resources/kankuro_esquerda2.png"));
+				}
+				posX -= 33;
+			}
+			if(i >= 26 && i <= 31) {
+				if (i % 2 == 0) {
+					img = new ImageIcon(Panel.class.getResource("/resources/kankuro_costa1.png"));
+				} else {
+					img = new ImageIcon(Panel.class.getResource("/resources/kankuro_costa2.png"));
+				}
+				posY -= 34;
+			}
+			if(i >= 31 && i < 37) {
+				if (i % 2 == 0) {
+					img = new ImageIcon(Panel.class.getResource("/resources/kankuro_esquerda1.png"));
+				} else {
+					img = new ImageIcon(Panel.class.getResource("/resources/kankuro_esquerda2.png"));
+				}
+				posX -= 33;
+			}
+			if(i >= 37 && i < 39) {
+				if (i % 2 == 0) {
+					img = new ImageIcon(Panel.class.getResource("/resources/kankuro_costa1.png"));
+				} else {
+					img = new ImageIcon(Panel.class.getResource("/resources/kankuro_costa2.png"));
+				}
+				posY -= 34;
+			}
+			if(i >= 39 && i <= 44) {
+				if (i % 2 == 0) {
+					img = new ImageIcon(Panel.class.getResource("/resources/kankuro_direita1.png"));
+				} else {
+					img = new ImageIcon(Panel.class.getResource("/resources/kankuro_direita2.png"));
+				}
+				posX += 33;
+			}
+			if(i >= 44 && i < 50) {
+				if (i % 2 == 0) {
+					img = new ImageIcon(Panel.class.getResource("/resources/kankuro_costa1.png"));
+				} else {
+					img = new ImageIcon(Panel.class.getResource("/resources/kankuro_costa2.png"));
+				}
+				posY -= 34;
+			}
+			if(i >= 50 && i < 51) {
+				if (i % 2 == 0) {
+					img = new ImageIcon(Panel.class.getResource("/resources/kankuro_direita1.png"));
+				} else {
+					img = new ImageIcon(Panel.class.getResource("/resources/kankuro_direita2.png"));
+				}
+				posX += 33;
+			}
+			if(i >= 51 && i < 57) {
+				if (i % 2 == 0) {
+					img = new ImageIcon(Panel.class.getResource("/resources/kankuro_frente1.png"));
+				} else {
+					img = new ImageIcon(Panel.class.getResource("/resources/kankuro_frente2.png"));
+				}
+				posY += 34;
+			}
+			
+			sleep();
 			peca.setIcon(img);
-			posY += 10;
-			posX -= 5;
-			peca.setLocation(posX + i, posY + i);
+			peca.setLocation(posX, posY);
+
 		}
 		peca.setIcon(new ImageIcon(Panel.class.getResource("/resources/kankuro_frente1.png")));
 	}
+
 	public void sleep() {
 		try {
-			TimeUnit.MILLISECONDS.sleep(500);
+			TimeUnit.MILLISECONDS.sleep(300);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
