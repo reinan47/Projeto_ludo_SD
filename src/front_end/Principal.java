c25466dd06fdcae7b4b9da195e2d30a86a4b55ac
Reinan <reinanzinho100@gmail.com>
2023-04-28 03:12:27 -0300

    criada a tela inicial

---
+++ src/front_end/Principal.java
@@ -0,0 +1,38 @@
+package front_end;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JInternalFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class Principal extends JPanel{
+	public Principal() {
+	}
+
+	public void eventoJogar(JButton jogar) {
+		jogar.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Panel contentPane = new Panel();
+				contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+				contentPane.revalidate();
+				contentPane.repaint();
+			}
+		});
+	}
+	
+	public void eventoEncontrarPartida(JButton encontrarPartida) {
+		encontrarPartida.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JInternalFrame jif = new JInternalFrame();
+				
+			}
+		});
+	}
+}
--- src/front_end/frame.java
+++ src/front_end/frame.java
@@ -1,12 +1,17 @@
 package front_end;
 
-import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.EventQueue;
-
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.WindowConstants;
-import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import java.awt.Font;
+import javax.swing.SwingConstants;
 
 public class frame extends JFrame {
 
@@ -14,7 +19,19 @@ public class frame extends JFrame {
 	 *
 	 */
 	private static final long serialVersionUID = 1L;
-	Panel contentPane = new Panel();
+	private Principal principal = new Principal();
+
+	private JLabel ip = new JLabel("IP");
+	private JLabel fundo = new JLabel(new ImageIcon(frame.class.getClassLoader().getResource("naruto.png")));
+	private JLabel port = new JLabel("Porta");
+	private JTextField ipValue = new JTextField();
+	
+	private JTextField portValue = new JTextField();
+	
+	
+	private JButton criarPartida = new JButton("Criar Partida");
+	private JButton encontrarPartida = new JButton("Entrar em partida");
+	private JButton jogar = new JButton("Jogar");
 
 	/**
 	 * Launch the application.
@@ -24,7 +41,7 @@ public class frame extends JFrame {
 			@Override
 			public void run() {
 				try {
-					frame frame = new frame();					
+					frame frame = new frame();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -37,13 +54,118 @@ public class frame extends JFrame {
 	 * Create the frame.
 	 */
 	public frame() {
+		principal.setLocale(null);
+		principal.setLayout(null);
 		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		setBounds(150, 10, 1000, 700);
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setBounds(400, 10, 500, 700);
+		setContentPane(principal);
+		criarPartida.setForeground(Color.WHITE);
+		criarPartida.setBackground(Color.BLACK);
+		criarPartida.setFont(new Font("Franklin Gothic Demi Cond", Font.PLAIN, 14));
+		criarPartida.setBounds(170, 550, 150, 25);
+		principal.add(criarPartida);
+		encontrarPartida.setForeground(Color.WHITE);
+		encontrarPartida.setBackground(Color.BLACK);
+		encontrarPartida.setFont(new Font("Franklin Gothic Demi Cond", Font.PLAIN, 14));
+		encontrarPartida.setBounds(170, 400, 150, 25);
+		principal.add(encontrarPartida);
+		EventoJogar(principal);
+		fundo.setBackground(Color.BLACK);
+		fundo.setText("porta");
 		
-		setContentPane(contentPane);
+		// parte q add para se conectar a partida
+		fundo.setBounds(0, 0, 500, 700);
+		ipValue.setBackground(new Color(234, 234, 234));
+		ipValue.setHorizontalAlignment(SwingConstants.CENTER);
+		ipValue.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+
+		ipValue.setBounds(150, 162, 200, 25);
+		portValue.setBackground(new Color(234, 234, 234));
+		portValue.setHorizontalAlignment(SwingConstants.CENTER);
+		portValue.setBounds(207, 262, 80, 25);
+		ip.setForeground(new Color(0, 0, 0));
+		ip.setToolTipText("");
+		ip.setBackground(Color.BLACK);
+		ip.setFont(new Font("Franklin Gothic Demi Cond", Font.BOLD, 14));
+		ip.setHorizontalAlignment(SwingConstants.CENTER);
 		
+		ip.setBounds(150, 134, 200, 25);
+		port.setForeground(new Color(0, 0, 0));
+		port.setHorizontalAlignment(SwingConstants.CENTER);
+		port.setFont(new Font("Franklin Gothic Demi Cond", Font.BOLD, 14));
+		port.setBackground(Color.BLACK);
+		port.setBounds(207, 236, 80, 25);
 		
+		ip.setVisible(false);
+		port.setVisible(false);
+		ipValue.setVisible(false);
+		portValue.setVisible(false);
+		jogar.setForeground(Color.WHITE);
+		jogar.setBackground(Color.BLACK);
+		jogar.setFont(new Font("Franklin Gothic Demi Cond", Font.PLAIN, 15));
+		
+		jogar.setFocusPainted(false);
+		jogar.setContentAreaFilled(true);
+		
+		criarPartida.setFocusPainted(false);
+		criarPartida.setContentAreaFilled(true);
+		
+		encontrarPartida.setFocusPainted(false);
+		encontrarPartida.setContentAreaFilled(true);
+		
+		jogar.setBounds(202, 310, 90, 25);
+		jogar.setVisible(false);
+		getContentPane().add(ip);
+		getContentPane().add(port);
+		getContentPane().add(ipValue);
+		getContentPane().add(portValue);
+		getContentPane().add(jogar);
+		getContentPane().add(fundo);
+		
+		EventoEntrarPartida();
 	}
 
+	public void EventoJogar(Principal principal) {
+		criarPartida.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Panel contentPane = new Panel();
+				principal.eventoJogar(criarPartida);
+				setContentPane(contentPane);
+				setBounds(150, 10, 1000, 700);
+				// implementar logica para startar server
+
+			}
+		});
+	}
+
+	public void EventoEntrarPartida() {
+		encontrarPartida.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				ipValue.setBounds(150, 162, 200, 25);
+				portValue.setBounds(207, 262, 80, 25);
+				ip.setOpaque(false);
+				ip.setBackground(Color.BLACK);
+				
+				
+				ip.setBounds(240, 122, 25, 50);
+				port.setBounds(230, 215, 35, 50);
+				ip.setVisible(true);
+				port.setVisible(true);
+				ipValue.setVisible(true);
+				portValue.setVisible(true);
+				jogar.setVisible(true);
+			}
+		});
+	}
+		
+		public void EventoJogarPorIpPort() {
+			jogar.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					//implementar pra entrar por porta e ip
+				}
+			});
+	}
 }
